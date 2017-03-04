4734752a9548fd5ad4182e4bdb151daeba76683f
Forrest Collman <forrestc@alleninstitute.org>
2017-03-03 17:25:19 -0800

    reorganizing

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+with open('requirements.txt', 'r') as f:
+    required = f.read().splitlines()
+
+setup(name='json_module',
+      version='1.0',
+      description=' a wrapper for setting up modules that can have parameters specified by command line arguments,\
+       json_files, or dictionary objects. Providing a common wrapper for data processing modules.',
+      author='Forrest Collman,David Feng',
+      author_email='forrestc@alleninstitute.org',
+      packages=['json_module'],
+      install_requires=required)
