84d325a1188ba98996601f2f6dbb3ab06dcda4c5
Forrest Collman <forrestc@alleninstitute.org>
2017-03-08 11:37:29 -0800

    changing test configuration

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from setuptools import setup
 with open('requirements.txt', 'r') as f:
     required = f.read().splitlines()
 
+with open('test/test_requirements.txt','r') as f:
+    test_required = f.read().splitlines()
+
 setup(name='json_module',
       version='1.0',
       description=' a wrapper for setting up modules that can have parameters specified by command line arguments,\
@@ -10,4 +13,6 @@ setup(name='json_module',
       author='Forrest Collman,David Feng',
       author_email='forrestc@alleninstitute.org',
       packages=['json_module'],
-      install_requires=required)
+      install_requires=required,
+      setup_requires=['pytest-runner'],
+      tests_require=test_required)
