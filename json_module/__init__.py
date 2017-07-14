ced98e17a4123b45a0d6c9567efab79a5110982f
forrest collman <forrest.collman@gmail.com>
2017-07-14 10:31:23 -0700

    changed relative imports

--- json_module/__init__.py
+++ json_module/__init__.py
@@ -1,6 +1,6 @@
-from fields import InputFile, InputDir, OutputFile, OptionList
-from schemas import ModuleParameters
-from json_module import JsonModule
+from .fields import InputFile, InputDir, OutputFile, OptionList
+from .schemas import ModuleParameters
+from .json_module import JsonModule
 
 def main():
     jm = JsonModule()
