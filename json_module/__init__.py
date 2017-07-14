d1301ad19dba4313ab7fb1c9aa0c99aeee2817af
forrest collman <forrest.collman@gmail.com>
2017-07-14 11:50:01 -0700

    python 3 print

--- json_module/__init__.py
+++ json_module/__init__.py
@@ -3,8 +3,9 @@ from .fields import InputFile, InputDir, OutputFile, OptionList
 from .schemas import ModuleParameters
 from .json_module import JsonModule
 
+
 def main():
     jm = JsonModule()
-    print jm.args
+    print(jm.args)
 
 if __name__ == "__main__": main()
