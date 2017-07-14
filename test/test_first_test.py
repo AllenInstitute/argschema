0a85add364113e07b1258c806d1369f2467e71d7
forrest collman <forrest.collman@gmail.com>
2017-07-14 10:29:06 -0700

    added new tests

--- test/test_first_test.py
+++ test/test_first_test.py
@@ -1,5 +1,6 @@
 import pytest
 from json_module import JsonModule, ModuleParameters
+from json_module.fields import InputFile, OutputFile
 import tempfile
 import os
 import json
@@ -38,7 +39,6 @@ def test_log_catch():
         jm = JsonModule(input_data=example,args=[])
         print jm.args
 
-
 class TestExtension(mm.Schema):
     a = mm.fields.Str(metadata={'description':'a string'})
     b = mm.fields.Int(metadata={'description':'an integer'}) 
@@ -48,11 +48,38 @@ class TestExtension(mm.Schema):
 class SimpleExtension(ModuleParameters):
     test = mm.fields.Nested(TestExtension)
 
+class BasicInputFile(ModuleParameters):
+    input_file = InputFile(required=True, metadata={'description':'a simple file'})
+
+class BasicOutputFile(ModuleParameters):
+    output_file = OutputFile(required=True,metadata={'decription':'a simple output file'})
+
+input_file_example ={
+    'input_file':'relative.file'
+}
+
+output_file_example ={
+    'output_file':'output.file'
+}
+
+def test_relative_file_input():
+    fp = open(input_file_example['input_file'],'w')
+    fp.write("test")
+    fp.close()
+    mod = JsonModule(input_data = input_file_example, schema_type = BasicInputFile,args=[])
+    os.remove(input_file_example['input_file'])
+def test_relative_file_input_failed():
+    with pytest.raises(mm.ValidationError):
+        mod = JsonModule(input_data = input_file_example, schema_type = BasicInputFile,args=[])
+    
+def test_output_file_relative():
+    mod = JsonModule(input_data = output_file_example, schema_type = BasicOutputFile,args=[])
 
 def test_simple_extension_required():
     with pytest.raises(mm.ValidationError):
         example1 = {}
         mod = JsonModule(input_data=example1,schema_type = SimpleExtension,args=[])
+
 SimpleExtension_example_invalid={
     'test':
     {
@@ -74,12 +101,10 @@ def test_simple_extension_fail():
         mod = JsonModule(input_data=SimpleExtension_example_invalid,schema_type=SimpleExtension,args=[])
 
 def test_simple_extension_pass(): 
-
     mod = JsonModule(input_data=SimpleExtension_example_valid,schema_type=SimpleExtension,args=[])
     assert mod.args['test']['a']=='hello'
     assert mod.args['test']['b']==1
     assert len(mod.args['test']['d'])==3
-
 
 def test_simple_extension_write_pass(tmpdir):
     file = tmpdir.join('testinput.json')
