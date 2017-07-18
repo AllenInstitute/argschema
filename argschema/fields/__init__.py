7045afd0d691d9005f11b092222ee09589d0ec92
RussTorres <russtorres@gmail.com>
2017-07-18 15:55:06 -0700

    init: allow all marshmallow field types to be accesible from argschema

--- argschema/fields/__init__.py
+++ argschema/fields/__init__.py
@@ -1,6 +1,8 @@
 '''sub-module for custom marshmallow fields of general utility'''
+from marshmallow.fields import *
+from marshmallow.fields import __all__
 from .files import OutputFile, InputDir, InputFile
 from .numpyarrays import NumpyArray
 from .options import OptionList
 
-__all__ = ['OutputFile', 'InputDir', 'InputFile', 'NumpyArray', 'OptionList']
+__all__ += ['OutputFile', 'InputDir', 'InputFile', 'NumpyArray', 'OptionList']
