fab1b8ae9f5ef6638875b51c0d22a0c7b9d108a2
RussTorres <russtorres@gmail.com>
2017-07-18 12:53:43 -0700

    forgot to update init with numpy->numparray

--- argschema/fields/__init__.py
+++ argschema/fields/__init__.py
@@ -1,6 +1,6 @@
 '''sub-module for custom marshmallow fields of general utility'''
-from files import OutputFile, InputDir, InputFile
-from numpy import NumpyArray
-from options import OptionList
+from .files import OutputFile, InputDir, InputFile
+from .numpyarrays import NumpyArray
+from .options import OptionList
 
 __all__ = ['OutputFile', 'InputDir', 'InputFile', 'NumpyArray', 'OptionList']
