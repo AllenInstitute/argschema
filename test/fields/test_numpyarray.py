1a68e65e5c077bf97abe1fb28eaa82d767acd1d0
forrest collman <forrest.collman@gmail.com>
2017-07-19 08:21:09 -0700

    added more numpy tests

--- test/fields/test_numpyarray.py
+++ test/fields/test_numpyarray.py
@@ -21,3 +21,18 @@ def test_numpy():
         input_data=numpy_array_test, schema_type=NumpyFileuint16, args=[])
     assert mod.args['a'].shape == (2, 2)
     assert mod.args['a'].dtype == 'uint16'
+
+def test_bad_shape():
+    bad_shape = {
+        'a':[[1,2],[3]]
+    }
+    with pytest.raises(mm.ValidationError):
+        mod = ArgSchemaParser(
+            input_data=bad_shape, schema_type=NumpyFileuint16, args=[])
+def test_bad_data():
+    bad_shape = {
+        'a':[['a','b']]
+    }
+    with pytest.raises(mm.ValidationError):
+        mod = ArgSchemaParser(
+            input_data=bad_shape, schema_type=NumpyFileuint16, args=[])
