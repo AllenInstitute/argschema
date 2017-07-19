b0d4eb3ee3ab7cf77132a50db799ed92fc8dd3d5
forrest collman <forrest.collman@gmail.com>
2017-07-19 08:17:25 -0700

    converting loglevel to string to avoid unneccesary serialize routine

--- argschema/fields/loglevel.py
+++ argschema/fields/loglevel.py
@@ -1,7 +1,7 @@
 import logging
 import marshmallow as mm
 
-class LogLevel(mm.fields.Field):
+class LogLevel(mm.fields.Str):
     '''LogLevel is a field type that provides a setting for the loglevel of
     python.logging.  This class will both validate the input and also *set* the
     input globally.  In simple scenarios, a module will not have to do any
@@ -13,9 +13,6 @@ class LogLevel(mm.fields.Field):
         kwargs['metadata']=kwargs.get('metadata',{'description': 'set log level'})
         kwargs['default']=kwargs.get('default','WARN')
         super(LogLevel, self).__init__(*args,**kwargs)
-
-    def _serialize(self, value, attr, obj):
-        return value;
 
     def _validate(self, value):
         if not hasattr(logging, value) or type(getattr(logging, value)) is not int:
