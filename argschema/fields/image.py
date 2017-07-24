b8ed0c5a884107086548a3141a3a282aef96ee20
John Galbraith <johng@alleninstitute.org>
2017-07-24 12:55:10 -0700

    Add ImageBatch tests

--- argschema/fields/image.py
+++ argschema/fields/image.py
@@ -10,15 +10,15 @@ class ImageBatch(mm.fields.Field):
     def _validate(self, imagelist):
         format_map = collections.Counter([imghdr.what(str(p)) for p in imagelist])
         if not format_map:
-            raise mm.ValidationError('{} contains no supported images'.format(path))
+            raise mm.ValidationError('no supported images found')
         for imagetype, count in format_map.items():
             logging.info('batching {} {} images'.format(count, imagetype))
 
     def _deserialize(self, value, attr, obj):
         logging.debug('queuing image batch from {}'.format(value))
-        path = Path(value)
+        path = Path(str(value))
         if not path.is_dir():
             raise mm.ValidationError('{} is not a valid directory'.format(path))
 
-        return [file for file in path.iterdir() if imghdr.what(str(file))]
+        return sorted([file for file in path.iterdir() if imghdr.what(str(file))])
  
