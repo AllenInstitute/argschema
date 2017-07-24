a992a19d4293de3d2265cc0d37f05326f5303593
John Galbraith <johng@alleninstitute.org>
2017-07-24 10:51:59 -0700

    Add ImageBatch field type

---
+++ argschema/fields/image.py
@@ -0,0 +1,24 @@
+from pathlib import Path
+import marshmallow as mm
+import collections, imghdr, logging
+
+class ImageBatch(mm.fields.Field):
+    """Specify a batch of images.  The input value should be a directory name
+    containing supported images. The images should be sortable by filename.
+    """
+
+    def _validate(self, imagelist):
+        format_map = collections.Counter([imghdr.what(str(p)) for p in imagelist])
+        if not format_map:
+            raise mm.ValidationError('{} contains no supported images'.format(path))
+        for imagetype, count in format_map.items():
+            logging.info('batching {} {} images'.format(count, imagetype))
+
+    def _deserialize(self, value, attr, obj):
+        logging.debug('queuing image batch from {}'.format(value))
+        path = Path(value)
+        if not path.is_dir():
+            raise mm.ValidationError('{} is not a valid directory'.format(path))
+
+        return [file for file in path.iterdir() if imghdr.what(str(file))]
+ 
