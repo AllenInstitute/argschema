b10dd5734ad667904d9ec6901d8833127c7724bf
forrest collman <forrest.collman@gmail.com>
2017-07-30 09:58:49 -0700

    testing tox system by failing python3

--- test/test_first_test.py
+++ test/test_first_test.py
@@ -6,6 +6,8 @@ import marshmallow as mm
 from argschema import ArgSchemaParser, ArgSchema
 import argschema
 
+def test_fail_python3():
+    print 'bad python3'
 
 def test_bad_path():
     with pytest.raises(mm.ValidationError):
