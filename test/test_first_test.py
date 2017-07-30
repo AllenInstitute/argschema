e66f7c4e7882a31b64762c77ffc91a438bd8f021
forrest collman <forrest.collman@gmail.com>
2017-07-30 10:02:41 -0700

    removing intentional bad python3 test

--- test/test_first_test.py
+++ test/test_first_test.py
@@ -6,8 +6,6 @@ import marshmallow as mm
 from argschema import ArgSchemaParser, ArgSchema
 import argschema
 
-def test_fail_python3():
-    print 'bad python3'
 
 def test_bad_path():
     with pytest.raises(mm.ValidationError):
