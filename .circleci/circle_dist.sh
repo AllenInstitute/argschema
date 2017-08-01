341a50548f0aef12ac3a47830d76e93df26429d8
forrest collman <forrest.collman@gmail.com>
2017-07-31 21:03:45 -0700

    switching to master only

--- .circleci/circle_dist.sh
+++ .circleci/circle_dist.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-if [ -z "$CI" ]; then
-    echo "Will only continue on CI"
-    exit
-fi
-
-if [[ $CIRCLE_BRANCH != "auto_deploy" ]]; then
-    echo "Will only continue for master builds"
-    exit
-fi
-
 # build package and upload to private pypi index
 echo "[pypi]" >> ~/.pypirc
 echo "username=$PYPI_USERNAME" >> ~/.pypirc
