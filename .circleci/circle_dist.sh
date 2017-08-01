9afca9d03c33b573c88e7ffebf42420f446238c5
forrest collman <forrest.collman@gmail.com>
2017-07-31 20:50:50 -0700

    adding universal flag

--- .circleci/circle_dist.sh
+++ .circleci/circle_dist.sh
@@ -15,5 +15,5 @@ echo "[pypi]" >> ~/.pypirc
 echo "username=$PYPI_USERNAME" >> ~/.pypirc
 echo "password=$PYPI_PASSWORD" >> ~/.pypirc
 
-python setup.py bdist_wheel
+python setup.py bdist_wheel --universal
 twine upload dist/*
