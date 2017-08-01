c57ad4d3b018fff9cfd36657df4e75d07fa1f576
forrest collman <forrest.collman@gmail.com>
2017-07-31 20:33:02 -0700

    adding script for packaging

---
+++ .circleci/circle_dist.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+if [ -z "$CI" ]; then
+    echo "Will only continue on CI"
+    exit
+fi
+
+if [[ $CIRCLE_BRANCH != "auto_deploy" ]]; then
+    echo "Will only continue for master builds"
+    exit
+fi
+
+# build package and upload to private pypi index
+echo "[pypi-private]" >> ~/.pypirc
+echo "username=$PYPI_USERNAME" >> ~/.pypirc
+echo "password=$PYPI_PASSWORD" >> ~/.pypirc
+
+python setup.py bdist wheel
+twine upload dist/*
