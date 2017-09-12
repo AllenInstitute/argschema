36d6f067ac18a0a523706298a096e97a9b2cb87d
forrest collman <forrest.collman@gmail.com>
2017-09-12 12:30:27 -0700

    first attempt at a rst table

--- argschema/autodoc.py
+++ argschema/autodoc.py
@@ -36,15 +36,43 @@ def process_schemas(app, what, name, obj, options, lines):
         if issubclass(obj,mm.Schema):
             #but document all mm.Schema's
             schema = obj()
-            lines.append(".. note::")
-            lines.append("")
-            lines.append("  keys: (field_type \: raw_type) description")
+            #lines.append(".. note::")
+            #lines.append("")
+            #lines.append("  keys: (field_type \: raw_type) description")
+            lines.append(".. csv-table:: %s"%obj.__name__)
+            lines.append('   :header: "key", "description", "default","type"')
+            lines.append('   :widths: 30, 80, 30, 30')
+            lines.append('')
+            
 
-            #loop over the declared fields for this schema
+            # #loop over the declared fields for this schema
             for field_name, field in schema.declared_fields.items():
                 #we will build up the documentation line for each field
                 #start declaring the key as field_name like a parameter
-                field_line = "    :%s: "%field_name
+                field_line = "    %s,"%field_name
+               
+                #get the description of this field and add it to documentation
+                description = get_description_from_field(field)
+                
+                if description is None:
+                    description="no description"         
+                field_line+='"%s",'%description
+    
+
+                #if field.required:
+                #    #add a REQUIRED stamp to required fields
+                #    field_line += " REQUIRED "
+                if field.default is not mm.missing:
+                    #add in the default value if there is one
+                    default = '"{}",'.format(field.default)
+                    field_line += default
+                else:
+                    field_line += "NA,"
+                    
+             
+               
+                
+                #add this field line to the documentation
                 try:
                     #get the set of types this field was derived from
                     if isinstance(field, mm.fields.List):
@@ -52,7 +80,8 @@ def process_schemas(app, what, name, obj, options, lines):
                         base_types = inspect.getmro(type(field.container))
                     else:
                         base_types = inspect.getmro(type(field))
-                  
+            
+                
                     #use these base_types to figure out the raw_json type for this field
                     if isinstance(field,mm.fields.Nested):
                         #if it's a nested field we should specify it as a dict, and link to the documentation 
@@ -72,25 +101,10 @@ def process_schemas(app, what, name, obj, options, lines):
                             #if its not in the FIELD_TYPE_MAP, we aren't sure what type it is
                             #TODO handle this more elegantly/and/or patch up more use cases
                             raw_type = '?'
-                    field_line += "(:class:`{}.{}` : {}) ".format(type(field).__module__,type(field).__name__,raw_type)
+                    field_line += ":class:`{}.{}` : {}".format(type(field).__module__,type(field).__name__,raw_type)
                 except:
                     #in case this fails for some reason, note it as unknown
                     #TODO handle this more elegantly, identify and patch up such cases
-                    field_line += " (unknown) "
-
-                #get the description of this field and add it to documentation
-                description = get_description_from_field(field)
-                if description is not None:
-                    field_line+=description
-                else:
-                    #if there is no description note that
-                    field_line+="no description "
-                if field.required:
-                    #add a REQUIRED stamp to required fields
-                    field_line += " REQUIRED "
-                if field.default is not mm.missing:
-                    #add in the default value if there is one
-                    field_line += " (default = {})".format(field.default)
-                #add this field line to the documentation
+                    field_line += "unknown"
                 lines.append(field_line)
-            
+            #lines.append(table_line)
