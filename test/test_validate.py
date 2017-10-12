2db265b4b431d8e0098d0db20819e7bbc23056c7
Jed Perkins <perkins.jed@gmail.com>
2017-10-12 16:45:42 -0700

    Expand tests to cover None in shape

--- test/test_validate.py
+++ test/test_validate.py
@@ -8,6 +8,8 @@ import numpy as np
 class MySchema(ArgSchema):
     a = NumpyArray(dtype='float', description='Test input array schema',
                    validate=validate.Shape((2,2)))
+    b = NumpyArray(dtype='float', description='Test array',
+                   validate=validate.Shape((2, None)))
 
 
 @pytest.mark.parametrize("invalid_shape", [
@@ -24,6 +26,7 @@ def test_shape_init_invalid(invalid_shape):
     ([100, 5], (100, 5)),
     ((20, 20, 30, 80), (20, 20, 30, 80)),
     ([50], (50,)),
+    ((2, None, 3), (2, None, 3)),
 ])
 def test_shape_init(input_shape, expected):
     validator = validate.Shape(input_shape)
@@ -34,7 +37,7 @@ def test_shape_init(input_shape, expected):
     ((2, 2), "notanarray"),
     ((5, 5, 3), 6),
     ((3, 3), [[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
-    ((2, 2), np.empty((2,3)))
+    ((2, 2), np.empty((2,3))),
 ])
 def test_shape_call_invalid(validation_shape, input_array):
     validator = validate.Shape(validation_shape)
@@ -44,15 +47,22 @@ def test_shape_call_invalid(validation_shape, input_array):
 
 @pytest.mark.parametrize("validation_shape,input_array", [
     ((2, 2), np.empty((2,2))),
+    ((2, 3, None), np.empty((2, 3, 5)))
 ])
 def test_shape_call(validation_shape, input_array):
     validator = validate.Shape(validation_shape)
     assert(validator(input_array))
 
 
-def test_parser_validation():
-    i1 = {"a": [[1, 2], [3, 4]]}
-    p1 = ArgSchemaParser(input_data=i1, schema_type=MySchema)
-    i2 = {"a": [[1, 2, 3], [4, 5, 6]]}
-    with pytest.raises(mm.ValidationError):
-        p2 = ArgSchemaParser(input_data=i2, schema_type=MySchema)
+@pytest.mark.parametrize("input_dict,raises", [
+    ({"a": [[1, 2], [3, 4]]}, False),
+    ({"b": [[1, 2, 3, 4], [5, 6, 7, 8]]}, False),
+    ({"a": [[1, 2, 3], [4, 5, 6]]}, True),
+    ({"b": [[1, 2], [3, 4], [5, 6]]}, True),
+])
+def test_parser_validation(input_dict,raises):
+    if raises:
+        with pytest.raises(mm.ValidationError):
+            p = ArgSchemaParser(input_data=input_dict, schema_type=MySchema)
+    else:
+        p = ArgSchemaParser(input_data=input_dict, schema_type=MySchema)
