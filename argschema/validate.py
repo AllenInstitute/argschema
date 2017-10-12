45857b414a2f6e8fd83d5d1530e09c70d140ec45
Jed Perkins <perkins.jed@gmail.com>
2017-10-12 16:45:12 -0700

    Update shape validator to allow None wildcard for length

--- argschema/validate.py
+++ argschema/validate.py
@@ -12,12 +12,14 @@ class Shape(Validator):
     Parameters
     ----------
     shape : tuple
-        Tuple specifying the required shape
+        Tuple specifying the required shape. If a value in the tuple is
+        `None`, any length in that dimension is valid.
 
     Raises
     ------
     ValueError
-        If the provided shape is not a valid tuple of integers
+        If the provided shape is not a valid tuple of integers and/or
+        None types
     marshmallow.ValidationError
         If the value being validated does not have a shape attribute
     """
@@ -27,19 +29,22 @@ class Shape(Validator):
             self.shape = tuple(shape)
         except TypeError:
             raise ValueError("{} is not a valid shape".format(shape))
-        if not all(isinstance(item, int) for item in self.shape):
+        if not all(isinstance(item, (int, type(None))) for item in self.shape):
             raise ValueError("{} is not a valid shape".format(shape))
         self.error = "Array shape is not {}".format(self.shape)
 
     def __call__(self, value):
         try:
-            valid = (value.shape == self.shape)
+            shape = value.shape
         except AttributeError:
             raise mm.ValidationError("{} is not a valid array, does not have "
                                      "attribute `shape`.".format(value))
+        if len(shape) != len(self.shape):
+            raise mm.ValidationError("Dimension mismatch: input shape {} does "
+                                     "not match {}.".format(shape, self.shape))
+        valid = all([a==b for a,b in zip(shape, self.shape) if b is not None])
         if not valid:
             raise mm.ValidationError("Array shape {} does not match required "
-                                     "shape {}.".format(value.shape,
-                                     self.shape))
+                                     "shape {}.".format(shape, self.shape))
         return valid
 
