e0c2d2273afe1d67de2c679db3c6d28b2650086d
Forrest Collman <forrest.collman@gmail.com>
2017-12-15 17:41:38 -0800

    added tests for multiple input configurations

--- test/sources/test_yaml.py
+++ test/sources/test_yaml.py
@@ -1,45 +1,60 @@
 import argschema
 from argschema.sources.yaml_source import YamlSource, YamlSink
+from argschema.sources.json_source import JsonSource, JsonSink
+from argschema.sources.source import MultipleConfiguredSourceError
 from argschema.argschema_parser import ArgSchemaYamlParser
 from test_classes import MySchema, MyOutputSchema
 import yaml
 import pytest
-
+import json
 
 class MyParser(ArgSchemaYamlParser):
     default_schema = MySchema
     default_output_schema = MyOutputSchema
 
+class MyDualParser(MyParser):
+    input_config_map = [JsonSource, YamlSource]
+    output_config_map = [JsonSink, YamlSink]
+
+input_data = {
+    'a': 5,
+    'nest': {
+        'one': 7,
+        'two': False
+    }
+}
 
 @pytest.fixture(scope='module')
-def test_input_file(tmpdir_factory):
+def test_yaml_input_file(tmpdir_factory):
     file_in = tmpdir_factory.mktemp('test').join('test_input_yaml.yml')
-    input_data = {
-        'a': 5,
-        'nest': {
-            'one': 7,
-            'two': False
-        }
-    }
+   
     with open(str(file_in), 'w') as fp:
         yaml.dump(input_data, fp, default_flow_style=False)
     return str(file_in)
 
+@pytest.fixture(scope='module')
+def test_json_input_file(tmpdir_factory):
+    file_in = tmpdir_factory.mktemp('test').join('test_input_json.json')
+   
+    with open(str(file_in), 'w') as fp:
+        json.dump(input_data, fp)
+    return str(file_in)
 
-def test_yaml_source(test_input_file):
-    mod = MyParser(input_source=YamlSource(test_input_file), args=[])
+
+def test_yaml_source(test_yaml_input_file):
+    mod = MyParser(input_source=YamlSource(test_yaml_input_file), args=[])
 
 
-def test_yaml_source_command(test_input_file):
-    mod = MyParser(args=['--input_yaml', test_input_file])
+def test_yaml_source_command(test_yaml_input_file):
+    mod = MyParser(args=['--input_yaml', test_yaml_input_file])
 
 
-def test_yaml_sink(test_input_file, tmpdir):
+def test_yaml_sink(test_yaml_input_file, tmpdir):
     outfile = tmpdir.join('test_out.yml')
     output_data = {
         'a': 3
     }
-    mod = MyParser(input_source=YamlSource(test_input_file),
+    mod = MyParser(input_source=YamlSource(test_yaml_input_file),
                    output_sink=YamlSink(str(outfile)))
     mod.output(output_data)
 
@@ -47,3 +62,17 @@ def test_yaml_sink(test_input_file, tmpdir):
         d = yaml.load(fp)
     output_data['b'] = "my value"
     assert (output_data == d)
+
+def test_dual_parser(test_json_input_file,test_yaml_input_file):
+
+    mod = MyDualParser(args=['--input_yaml', test_yaml_input_file])
+    assert mod.args['a']==5
+    assert mod.args['nest']==input_data['nest']
+
+    mod = MyDualParser(args=['--input_json', test_json_input_file])
+    assert mod.args['a']==5
+    assert mod.args['nest']==input_data['nest']
+
+def test_dual_parser_fail(test_json_input_file,test_yaml_input_file):
+    with pytest.raises(MultipleConfiguredSourceError):
+        mod = MyDualParser(args=['--input_yaml', test_yaml_input_file, '--input_json', test_json_input_file])
