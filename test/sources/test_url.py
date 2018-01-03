5072bc792c2eb4ef31cd4bd2adfa7cb540058f91
Forrest Collman <forrest.collman@gmail.com>
2018-01-03 09:37:28 -0800

    pep8

--- test/sources/test_url.py
+++ test/sources/test_url.py
@@ -1,8 +1,10 @@
-import requests 
+import requests
 import mock
 from url_source import UrlArgSchemaParser, UrlSource, MySchema
 from argschema import ArgSchemaParser
 # This method will be used by the mock to replace requests.get
+
+
 def mocked_requests_get(*args, **kwargs):
     class MockResponse:
         def __init__(self, json_data, status_code):
@@ -14,23 +16,26 @@ def mocked_requests_get(*args, **kwargs):
 
     if args[0] == 'http://localhost:88/test.json':
         return MockResponse({
-                                'a':7,
-                                'nest':{
-                                    'one':7,
-                                    'two':False
-                                }
-                            }, 200)
-
-
+            'a': 7,
+            'nest': {
+                'one': 7,
+                'two': False
+            }
+        }, 200)
     return MockResponse(None, 404)
+
 
 @mock.patch('requests.get', side_effect=mocked_requests_get)
 def test_url_parser(mock_get):
-    input_source=UrlSource(input_host='localhost',input_port=88,input_url='test.json')
-    mod = ArgSchemaParser(schema_type=MySchema, input_source=input_source, args = [])
-    assert(mod.args['a']==7)
+    input_source = UrlSource(input_host='localhost',
+                             input_port=88, input_url='test.json')
+    mod = ArgSchemaParser(schema_type=MySchema,
+                          input_source=input_source, args=[])
+    assert(mod.args['a'] == 7)
+
 
 @mock.patch('requests.get', side_effect=mocked_requests_get)
 def test_url_parser_command_line(mock_get):
-    mod = UrlArgSchemaParser(args = ['--input_host','localhost','--input_port','88','--input_url','test.json'])
-    assert(mod.args['a']==7)
+    mod = UrlArgSchemaParser(
+        args=['--input_host', 'localhost', '--input_port', '88', '--input_url', 'test.json'])
+    assert(mod.args['a'] == 7)
