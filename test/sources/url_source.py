2a885bc7e341cbc863d626dc4b8aedf4a12d45af
Forrest Collman <forrest.collman@gmail.com>
2018-01-03 08:14:54 -0800

    added protocol to url_source_demo

--- test/sources/url_source.py
+++ test/sources/url_source.py
@@ -1,4 +1,4 @@
-from argschema.sources import ArgSource
+from argschema.sources import ArgSource, ArgSink
 from argschema.schemas import DefaultSchema
 from argschema.fields import Str,Int
 from argschema import ArgSchemaParser
@@ -9,15 +9,18 @@ class UrlSourceConfig(DefaultSchema):
     input_host = Str(required=True, description="host of url")
     input_port = Int(required=False, default=80, description="port of url")
     input_url = Str(required=True, description="location on host of input")
+    input_protocol = Str(required=False, default='http')
 
 class UrlSource(ArgSource):
     ConfigSchema = UrlSourceConfig
     def get_dict(self):
-        url = "http://{}:{}/{}".format(self.input_host,
+        url = "{}://{}:{}/{}".format(self.input_protocol,
+                                self.input_host,
                                 self.input_port,
-                                self.input_url)
+                                self.input_url)                                
         response = requests.get(url)
         return response.json()
+
 
 class UrlArgSchemaParser(ArgSchemaParser):
     default_configurable_sources = [UrlSource]
