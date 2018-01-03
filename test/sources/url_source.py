2908dca00f53d3aa39e48d29077a424fe213dfcc
Forrest Collman <forrest.collman@gmail.com>
2018-01-03 09:26:15 -0800

    added optional parameter to url_source test

--- test/sources/url_source.py
+++ test/sources/url_source.py
@@ -9,15 +9,16 @@ class UrlSourceConfig(DefaultSchema):
     input_host = Str(required=True, description="host of url")
     input_port = Int(required=False, default=80, description="port of url")
     input_url = Str(required=True, description="location on host of input")
-    input_protocol = Str(required=False, default='http')
+    input_protocol = Str(required=False, default='http', description="url protocol to use")
 
 class UrlSource(ArgSource):
     ConfigSchema = UrlSourceConfig
+
     def get_dict(self):
         url = "{}://{}:{}/{}".format(self.input_protocol,
-                                self.input_host,
-                                self.input_port,
-                                self.input_url)                                
+                                     self.input_host,
+                                     self.input_port,
+                                     self.input_url)                                
         response = requests.get(url)
         return response.json()
 
