e0e00507e2d90858f8955cf8e8a0c91bf88481d7
Jed Perkins <perkins.jed@gmail.com>
2018-01-30 13:37:48 -0800

    Add examples for use with command-line documentation

---
+++ examples/deprecated_example.py
@@ -0,0 +1,15 @@
+from argschema import ArgSchema, ArgSchemaParser
+from argschema.fields import List, Float
+
+
+class MySchema(ArgSchema):
+    list_old = List(Float, default=[1.1, 2.2, 3.3],
+                    description="float list with deprecated cli")
+    list_new = List(Float, default=[4.4, 5.5, 6.6],
+                    cli_as_single_argument=True,
+                    description="float list with supported cli")
+
+
+if __name__ == '__main__':
+    mod = ArgSchemaParser(schema_type=MySchema)
+    print(mod.args)
