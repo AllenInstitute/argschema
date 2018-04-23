b258001a883e3e2a50b2c3fe4a58ce9656e14b19
Forrest Collman <forrest.collman@gmail.com>
2018-04-23 08:22:03 -0700

    bump to 2.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('test_requirements.txt', 'r') as f:
         test_required = [i for i in test_required if 'pywin32' not in i]
 
 setup(name='argschema',
-      version='2.0.2',
+      version='4.0.0',
       description=' a wrapper for setting up modules that can have parameters specified by command line arguments,\
        json_files, or dictionary objects. Providing a common wrapper for data processing modules.',
       author='Forrest Collman, David Feng',
