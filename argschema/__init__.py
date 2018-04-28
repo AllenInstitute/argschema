0d35f2d0fe9e48a64d0896542d70ae2af4f916b4
Forrest Collman <forrest.collman@gmail.com>
2018-04-28 08:23:35 -0700

    removing more deprecated items

--- argschema/__init__.py
+++ argschema/__init__.py
@@ -2,7 +2,6 @@
 from .fields import InputFile, InputDir, OutputFile, OptionList # noQA:F401
 from .schemas import ArgSchema # noQA:F401
 from .argschema_parser import ArgSchemaParser # noQA:F401
-from .deprecated import JsonModule, ModuleParameters # noQA:F401
 
 
 def main():  # pragma: no cover
